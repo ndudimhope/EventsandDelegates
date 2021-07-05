40e56f65e34be8cf658827cc3835c70eaf14389a
Hope Ndudim <hope.ndudim@thebulb.africa>
2021-07-05 05:38:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EventsandDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            var video = new Video() { Title = "Video1" };
+            var videoEncoder = new VideoEncoder();
+            var mailService = new MailService();
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.Encode(video);
+        }
+    }
+}
---
+++ VideoEncoder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsandDelegates
+{
+    class VideoEncoder
+    {
+        //1. Define a Delegate
+        //2. Define an Event based on that Delegate
+        //3. Raise the Event
+
+        public delegate void VideoEncoderEventHandler(Object source, EventArgs args);
+
+        public event VideoEncoderEventHandler VideoEncoded;
+
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded();
+        }
+
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, EventArgs.Empty);
+
+        }
+    }
+}
